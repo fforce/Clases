55f2fddaee6fad499e67a5c934baed8ebd49e223
Digital House <francolqr@gmail.com>
2018-08-01 18:45:26 -0300

    fix

--- chat/src/reducers/index.js
+++ chat/src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 
 import LoginReducer from './LoginReducer'
 import UserReduce from './UserReduce'
-//este estado lo maneja tal reducer
+//este estado lo maneja tal reducera
 const rootReducer = combineReducers({
     LoginReducer,
     UserReduce
